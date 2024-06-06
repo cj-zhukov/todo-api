fb1119013b84db60ce4beb342deaa6b44ef96c00
Sergey Zhukov <szhukov@aligntech.com>
2024-06-06 09:07:04 +0300

    fixed bug when quering table

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,20 @@
-use std::sync::Arc;
-
+use anyhow::Result;
 // pub type Result<T> = core::result::Result<T, Error>;
 // pub type Error = Box<dyn std::error::Error>;
-use anyhow::Result;
-
 pub mod config;
 
-use axum::http::header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE};
-use axum::http::{HeaderValue, Method};
-use axum::routing::post;
-use axum::Json;
-use axum::{Router, routing::get};
+use axum::{Router, routing::get, Json};
 use axum::response::IntoResponse;
 use axum::extract::{Path, State};
 use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
+use sqlx::PgPool;
 use tower_http::cors::{Any, CorsLayer};
 use tower_http::trace::TraceLayer;
-use sqlx::PgPool;
 use tracing_subscriber::{filter::LevelFilter, fmt, prelude::*, EnvFilter};
 
 const MAX_ROWS: u32 = 10;
 const TODO_TABLE_NAME: &str = "todos";
-
-// pub type AppStateRef = Arc<AppState>;
-// pub struct AppState {
-//     pub db: PgPool,
-// }
-
-// impl AppState {
-//     pub fn new(db: PgPool) -> AppStateRef {
-//         Arc::new(Self { db })
-//     }
-// }
 
 #[derive(Serialize, Deserialize, Debug, Clone, sqlx::FromRow)]
 pub struct Todo {
@@ -45,7 +27,7 @@ pub struct Todo {
 
 #[derive(Deserialize, Clone)]
 pub struct CreateTodo {
-    body: String,
+    pub body: String,
 }
 
 impl CreateTodo {
@@ -67,7 +49,7 @@ impl UpdateTodo {
 
     pub fn completed(&self) -> bool {
         self.completed
-}
+    }
 }
 
 impl Todo {
@@ -84,7 +66,7 @@ impl Todo {
     }
 
     pub async fn read(pool: PgPool, id: i64) -> Result<Todo> {
-        let sql = format!("select * from {} where id = ?", Self::table_name());
+        let sql = format!("select * from {} where id = $1", Self::table_name());
         let query = sqlx::query_as::<_, Self>(&sql);
         let data = query
             .bind(id)
@@ -104,7 +86,7 @@ impl Todo {
     }
 
     pub async fn query_table_to_json(pool: PgPool) -> Result<String> {
-        let sql = format!("select * from {} limit {};", Self::table_name(), MAX_ROWS);
+        let sql = format!("select * from {} limit {}", Self::table_name(), MAX_ROWS);
         let query = sqlx::query_as::<_, Self>(&sql);
         let data = query.fetch_all(&pool).await?;
         let res = serde_json::to_string(&data)?;
@@ -115,7 +97,7 @@ impl Todo {
 
 impl Todo {
     pub async fn create(pool: PgPool, new_todo: CreateTodo) -> Result<Todo> {
-        let sql = format!("insert into {} (body) values (?) returning *;", Self::table_name());
+        let sql = format!("insert into {} (body) values ($1) returning *", Self::table_name());
         let query = sqlx::query_as::<_, Self>(&sql);
         let data = query
             .bind(new_todo.body())
@@ -127,8 +109,9 @@ impl Todo {
 
     pub async fn update(pool: PgPool, id: i64, update_todo: UpdateTodo) -> Result<Todo> {
         let sql = format!("
-            update {} set body = ?, completed = ?,
-            updated_at = datetime('now') where id = ? returning *;", Self::table_name());
+            update {} 
+            set body = $1, completed = $2, updated_at = now()::timestamp 
+            where id = $3 returning *", Self::table_name());
         let query = sqlx::query_as::<_, Self>(&sql);
         let data = query
             .bind(update_todo.body())
@@ -140,15 +123,15 @@ impl Todo {
         Ok(data)
     }
 
-    pub async fn delete(pool: PgPool, id: i64) -> Result<()> {
-        let sql = format!("delete from {} where id = ?;", Self::table_name());
+    pub async fn delete(pool: PgPool, id: i64) -> Result<Todo> {
+        let sql = format!("delete from {} where id = $1 returning *", Self::table_name());
         let query = sqlx::query_as::<_, Self>(&sql);
-        let _data = query
+        let data = query
             .bind(id)
             .fetch_one(&pool)
             .await?;
         
-        Ok(())
+        Ok(data)
     }
 }
 
@@ -158,29 +141,28 @@ pub async fn todo_list(State(pool): State<PgPool>) -> Result<impl IntoResponse, 
     Ok(data)
 }
 
-
-pub async fn todo_read(State(pool): State<PgPool>, Path(id): Path<i64>) -> Result<Json<Todo>> {
-    let res = Todo::read(pool, id).await.map(Json::from)?;
-
-    Ok(res)
-}
-
-pub async fn todo_create(State(pool): State<PgPool>, Json(new_todo): Json<CreateTodo>) -> Result<Json<Todo>> {
-    let res = Todo::create(pool, new_todo).await.map(Json::from)?;
+pub async fn todo_read(State(pool): State<PgPool>, Path(id): Path<i64>) -> Result<impl IntoResponse, Json<Todo>> {
+    let res = Todo::read(pool, id).await.map(Json::from).unwrap();
 
     Ok(res)
 }
 
-pub async fn todo_update(State(pool): State<PgPool>, Path(id): Path<i64>, Json(updated_todo): Json<UpdateTodo>) -> Result<Json<Todo>> {
-    let res = Todo::update(pool, id, updated_todo).await.map(Json::from)?;
+pub async fn todo_create(State(pool): State<PgPool>, Json(new_todo): Json<CreateTodo>) -> Result<impl IntoResponse, Json<Todo>> {
+    let res = Todo::create(pool, new_todo).await.map(Json::from).unwrap();
 
     Ok(res)
 }
 
-pub async fn todo_delete(State(pool): State<PgPool>, Path(id): Path<i64>) -> Result<()> {
-    Todo::delete(pool, id).await?;
+pub async fn todo_update(State(pool): State<PgPool>, Path(id): Path<i64>, Json(updated_todo): Json<UpdateTodo>) -> Result<impl IntoResponse, Json<Todo>> {
+    let res = Todo::update(pool, id, updated_todo).await.map(Json::from).unwrap();
 
-    Ok(())
+    Ok(res)
+}
+
+pub async fn todo_delete(State(pool): State<PgPool>, Path(id): Path<i64>) -> Result<impl IntoResponse, Json<Todo>> {
+    let res = Todo::delete(pool, id).await.map(Json::from).unwrap();
+
+    Ok(res)
 }
 
 pub fn init_tracing() {
@@ -201,14 +183,15 @@ pub async fn create_router(pool: PgPool) -> Router {
     Router::new()
         .route("/alive", get(|| async { "ok" }))
         .route("/ready", get(ping)) 
-        .route("/todos", get(todo_list))
+        .route("/todos", get(todo_list).post(todo_create))
+        .route("/todos/:id", get(todo_read).put(todo_update).delete(todo_delete))
         .with_state(pool)
         .layer(CorsLayer::new().allow_methods(Any).allow_origin(Any))
         .layer(TraceLayer::new_for_http())
 }
 
 pub async fn ping() -> impl IntoResponse {
-    let msg = "foo";
+    let msg = "hello from todo-api";
 
     let json_response = serde_json::json!({
         "status": "success",
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ async fn main() -> Result<()> {
     let config = Config::new("config.json").await?;
     let pool = config.connect().await?;
     let router = create_router(pool).await;
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await?;
     axum::serve(listener, router).await?;
 
     Ok(())
